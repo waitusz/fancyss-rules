a5db391ffcfe4db1d22fc8dc038d259c7a9ea431
Oldman <22133928+waitusz@users.noreply.github.com>
2026-01-17 13:37:08 +0800

    Add files via upload

---
+++ rules_ng/rules.json.js
@@ -0,0 +1,86 @@
+{
+  "gfwlist": {
+    "name": "gfwlist.gz",
+    "date": "2026-01-13 00:12",
+    "md5": "22af83f63427a93cd3623c13002df0a4",
+    "count": "7180"
+  },
+  "chnlist": {
+    "name": "chnlist.gz",
+    "date": "2026-01-15 20:32",
+    "md5": "32a8b3efe92877142bfc336dc8b608b0",
+    "count": "114215",
+    "note": "merged from dnsmasq-china-list"
+  },
+  "chnroute": {
+    "name": "chnroute.txt",
+    "date": "2026-01-15 20:32",
+    "md5": "2e74dfa6905c04b017e382a90c7aca34",
+    "count": "8229",
+    "count_ip": "358495804",
+    "source": "merged"
+  },
+  "chnroute6": {
+    "name": "chnroute6.txt",
+    "date": "2026-01-15 20:32",
+    "md5": "2911412d099cf0456e45faf7a665fc92",
+    "count": "2040",
+    "source": "apnic"
+  },
+  "adslist": {
+    "name": "adslist.gz",
+    "date": "2026-01-13 00:12",
+    "md5": "8e7468a8007c26a9ca43c98a07fe793c",
+    "count": "107338",
+    "source": "anti-ad",
+    "url": "https://anti-ad.net/domains.txt"
+  },
+  "udplist": {
+    "name": "udplist.txt",
+    "date": "2026-01-06 07:47",
+    "md5": "0c06bd073a5d0ef749e781cf25862eaf",
+    "count": "46"
+  },
+  "rotlist": {
+    "name": "rotlist.txt",
+    "date": "2026-01-08 16:19",
+    "md5": "de470f08c946f80269b2b524efc93ce5",
+    "count": "28"
+  },
+  "white_list": {
+    "name": "white_list.txt",
+    "date": "2026-01-05 22:45",
+    "md5": "25f05424ba3ecb16e850a56b022f4ec4",
+    "count": "7"
+  },
+  "black_list": {
+    "name": "black_list.txt",
+    "date": "2026-01-06 00:02",
+    "md5": "ca21177ab36d888b1400cc0a4253f0fc",
+    "count": "1"
+  },
+  "block_list": {
+    "name": "block_list.txt",
+    "date": "2026-01-06 23:21",
+    "md5": "a98186b603e45e546122dc1c29ab0310",
+    "count": "898"
+  },
+  "apple_china": {
+    "name": "apple_china.txt",
+    "date": "2026-01-05 22:45",
+    "md5": "616b568666535fe67c8ca9b830fcaa24",
+    "count": "167"
+  },
+  "google_china": {
+    "name": "google_china.txt",
+    "date": "2026-01-05 22:45",
+    "md5": "2003b325844ddc1c5b4702c07c6864c0",
+    "count": "143"
+  },
+  "cdn_test": {
+    "name": "cdn_test.txt",
+    "date": "2026-01-05 22:45",
+    "md5": "866e71a7b3c9f6062bf4c5b4d6818fea",
+    "count": "89"
+  }
+}
